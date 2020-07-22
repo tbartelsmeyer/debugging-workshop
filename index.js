a1133c0462612c9be5b2cc3db13c34775c7b1af7
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-22 01:12:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
@@ -16,12 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   form.addEventListener('submit', (event) => {
-
+    event.preventDefault()
+    const username = document.getElementById('name-input').value
     if(username === "") return;
-    fetchJoke()
+    fetchJoke().then(joke => {
+    const newJokeLi = document.createElement('li')
     newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
     `
     jokeList.appendChild(newJokeLi)
+    })
   })
 })
